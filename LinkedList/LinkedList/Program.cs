8eeb0f97b22655ffc623a41a4835790f69dda3bc
unknown <kaithwas.697@gmail.com>
2022-03-27 13:35:31 +0530

    Created linked list

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedLists linkedList = new LinkedLists();
+            linkedList.AddNode(56);
+            linkedList.AddNode(30);
+            linkedList.AddNode(70);
+            linkedList.Display();
+
+
+
+        }
+    }
+}
