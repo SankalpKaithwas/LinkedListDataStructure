6d42476e31793122a95612ced72a1e3b166a2ce7
unknown <kaithwas.697@gmail.com>
2022-03-27 15:43:15 +0530

    Added feature to add elements in sorted order

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -6,21 +6,20 @@ namespace LinkedList
         static void Main(string[] args)
         {
             LinkedLists linkedList = new LinkedLists();
-
             linkedList.AddNode(30);
-            linkedList.AddNode(56);            
+            linkedList.AddNode(56);
             linkedList.AppendNode(70);
             linkedList.Insert(40, 30);
             linkedList.Display();
             linkedList.DeleteNode(40);
-            Console.WriteLine("\nSize is " +linkedList.getSize());
+            Console.WriteLine("\nSize is " + linkedList.getSize());
             linkedList.Display();
-            Console.WriteLine();           
+            Console.WriteLine();
             linkedList.DeleteEndNode();
             linkedList.Display();
 
             Node check = linkedList.Search(30);
-            if(check != null)
+            if (check != null)
             {
                 Console.WriteLine("\nNode found");
             }
@@ -28,9 +27,10 @@ namespace LinkedList
             {
                 Console.WriteLine("\nNode not found");
             }
-           
-
-
+            SortedLinkedList sortedLinkedList = new SortedLinkedList();
+            sortedLinkedList.SortedAppendNode(new int[] { 56, 30, 40, 70 });
+            Console.WriteLine("Sorted Linked List");
+            sortedLinkedList.Display();
 
         }
     }
---
+++ LinkedList/LinkedList/SortedLinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    internal class SortedLinkedList
+    {
+        public Node head;
+        public Node tail;
+        public SortedLinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void SortedAppendNode(params int[] data)
+        {
+            Array.Sort(data);
+            for (int i = 0; i < data.Length; i++)
+            {
+                AppendNode(data[i]);
+            }
+        }
+        public void AppendNode(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        internal void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.Write(" -> ");
+                }
+                temp = temp.next;
+            }
+        }
+    }
+}
